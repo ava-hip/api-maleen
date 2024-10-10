8bf59418ec5a0475f2fdc02bd4473c0bbeeac8ab
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2024-10-10 14:24:01 +0200

    first commit!

---
+++ src/main/java/fr/avahip/apimaleen/ApiMaleenApplication.java
@@ -0,0 +1,13 @@
+package fr.avahip.apimaleen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMaleenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMaleenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/avahip/apimaleen/ApiMaleenApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.avahip.apimaleen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMaleenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
